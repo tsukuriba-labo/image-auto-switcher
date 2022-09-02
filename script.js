6209852abc3a5e18c30c9a95d3e436ea3d394586
tamutamu <tamu.0.0.tamu@gmail.com>
2022-09-02 21:00:01 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("div.image-gallery").animate({ top: "-34vh" }, 100000);
+
+  $(".image-item img:nth-child(n+2)").hide();
+
+  $(".image-item img:first-child").each(function (index, elem) {
+    var interval = randomIntFromInterval(300, 1000) * 10;
+    console.log(interval);
+    setInterval(function () {
+      var _elem = $(".image-item img:first-child").eq(index);
+      $(_elem).fadeOut(2000);
+      $(_elem).next().fadeIn(2000);
+      $(_elem).appendTo($(_elem).parent().get(0));
+    }, interval);
+  });
+});
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
