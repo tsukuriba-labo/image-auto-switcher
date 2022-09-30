6a1fd8ea3f02b12296da9a593b77b16285631533
tamutamu <tamu.0.0.tamu@gmail.com>
2022-09-30 16:36:42 +0900

    update.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-  $("div.image-gallery").animate({ top: "-34vh" }, 100000);
+  //   $("div.image-gallery").animate({ top: "-34vh" }, 100000);
 
   $(".image-item img:nth-child(n+2)").hide();
 
   $(".image-item img:first-child").each(function (index, elem) {
-    var interval = randomIntFromInterval(300, 1000) * 10;
-    console.log(interval);
+    var interval = randomIntFromInterval(300, 1000) * 20;
     setInterval(function () {
       var _elem = $(".image-item img:first-child").eq(index);
       $(_elem).fadeOut(2000);
