377e728c48de423df5e32eb741b6c74923a2f3cc
tamutamu <tamu.0.0.tamu@gmail.com>
2022-09-30 17:40:22 +0900

    update.

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-$(document).ready(function () {
-  $("div.image-gallery").animate({ top: "-34vh" }, 100000);
+function setImageScroll(imageGroups) {
+  $(document).ready(function () {
+    // 画像スクロール
+    $("div.image-gallery").animate({ top: "-34vh" }, 100000);
 
-  $(".image-item img:nth-child(n+2)").hide();
+    // 二枚目以降の画像を非表示
+    $(".image-item img:nth-child(n+2)").hide();
 
-  $(".image-item[data-group^='w-'] img:first-child").each(function (
-    index,
-    elem
-  ) {
-    var interval_w = 5000;
-    var _elem = $(".image-item[data-group^='w-'] img:first-child").eq(index);
-    var wg = $(_elem).parent().attr("data-group");
+    let count = 0;
 
-    var num = wg.split("-")[1];
+    imageGroups.forEach(function (v, index) {
+      $(
+        ".image-item[data-group^='" + v.group + "-" + "'] img:first-child"
+      ).each(function (index, elem) {
+        var wg = $(elem).parent().attr("data-group");
+        var num = wg.split("-")[1];
 
-    setTimeout(function () {
-      $(_elem).fadeOut(2000);
-      $(_elem).next().fadeIn(2000);
-      $(_elem).appendTo($(_elem).parent().get(0));
+        var myCount = count;
 
-      if (num == "3") {
-        $("div.image-gallery").stop();
-      }
-    }, interval_w * num);
+        setTimeout(function () {
+          $(elem).fadeOut(2000);
+          $(elem).next().fadeIn(2000);
+          if (myCount == count - 1) {
+            $("div.image-gallery").stop();
+          }
+        }, v.interval * num);
+
+        // 合計カウント
+        count++;
+      });
+    });
   });
-});
+}
+
+setImageScroll([
+  { group: "w", interval: 5000 },
+  { group: "h", interval: 10000 },
+]);
