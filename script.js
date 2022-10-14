adc6b233338ed0a042156593d88c8f8152a1e56d
tamutamu <tamu.0.0.tamu@gmail.com>
2022-10-15 08:44:16 +0900

    refator: 完了、納品

--- script.js
+++ script.js
@@ -3,49 +3,57 @@
  *
  * @param {*} imageGroups
  */
-function setImageScroll(imageGroups) {
-  $(document).ready(function () {
-    // $("html,body").animate({ scrollTop: 0 }, 1);
-    const duration = 50000;
-    $(".image-gallery").clone(true).appendTo("div#container");
+function setImageScroll(
+  imageGroups,
+  _duration,
+  _mobileDuration,
+  breakPoint = "(min-width: 768px)"
+) {
+  var duration = _mobileDuration;
 
+  if (window.matchMedia(breakPoint).matches) {
+    duration = _duration;
+  }
+
+  // イメージギャラリ
+  var imggl1, imggl2;
+
+  function _setupScroll() {
     // 画像スクロール
-    var imggl1 = $(".image-gallery").eq(0);
-    var imggl2 = $(".image-gallery").eq(1);
+    imggl1 = $(".image-gallery").eq(0);
+    imggl2 = $(".image-gallery").eq(1);
+
+    imggl1.stop(false, false);
     imggl1.animate({ top: -1 * imggl1.height() - 10 }, duration, "linear");
 
+    imggl2.stop(false, false);
+    imggl2.css({ top: imggl1.position().top + imggl1.outerHeight(true) - 15 });
+    imggl2.animate({ top: -10 }, duration, "linear");
+  }
+
+  $(document).ready(function () {
     // 二枚目以降の画像を非表示
     $(".image-item img:nth-child(n+2)").hide();
 
     // イメージギャラリーを下にコピー
-    imggl2.css({ top: imggl1.position().top + imggl1.outerHeight(true) - 15 });
-    imggl2.animate({ top: -10 }, duration, "linear");
+    $(".image-gallery").clone(true).appendTo("div#container");
+
+    // 無限スクロール設定
+    _setupScroll();
 
     setInterval(() => {
-      var _imggl1 = $(".image-gallery").eq(0);
-      var _imggl2 = $(".image-gallery").eq(1);
-      var bottom = _imggl1.position().top + _imggl1.outerHeight(true);
+      var bottom = imggl1.position().top + imggl1.outerHeight(true);
 
       if (bottom < 0) {
-        _imggl1.appendTo("div#container");
-        _imggl1 = $(".image-gallery").eq(0);
-        _imggl2 = $(".image-gallery").eq(1);
+        // 上のイメージギャラリを下に移動
+        imggl1.appendTo("div#container");
 
-        _imggl1.stop(false, false);
-        _imggl1.animate(
-          { top: -1 * _imggl1.height() - 10 },
-          duration,
-          "linear"
-        );
-
-        _imggl2.stop(false, false);
-        _imggl2.css({
-          top: _imggl1.position().top + _imggl1.outerHeight(true) - 15,
-        });
-        _imggl2.animate({ top: -10 }, duration, "linear");
+        // 無限スクロール設定
+        _setupScroll();
       }
     }, 2000);
 
+    // 画像切り替え設定
     imageGroups.forEach(function (v, index) {
       $(".image-item[data-group^='" + v.group + "-" + "']").each(
         (index, elem) => {
@@ -66,10 +74,15 @@ function setImageScroll(imageGroups) {
     });
   });
 }
+
 // 画像スイッチ付きのスクロールをセットアップ
 // w-1, w-2, w-3,... のグループは5秒間隔で順に切り替わります。
 // h-1, h-2, h-3,... のグループは10秒間隔で順に切り替わります。
-setImageScroll([
-  { group: "w", interval: 5000 },
-  { group: "h", interval: 10000 },
-]);
+setImageScroll(
+  [
+    { group: "w", interval: 5000 },
+    { group: "h", interval: 10000 },
+  ],
+  50000,
+  300000
+);
